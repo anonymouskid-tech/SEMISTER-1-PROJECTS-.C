40fb74a5abf8d30fd85bee01f4c565aef6a43d3a
anonymouskid-tech <anonymouskaris20@gmail.com>
2024-09-25 14:02:33 +0300

    add the file

---
+++ new 1.c
@@ -0,0 +1,15 @@
+//program to find the area of a rectangle
+/*
+program to find area of a rectangle
+author:anonymous
+*/
+//processor scanf() ,printf*()
+#include<stdio.h>
+int main() {
+    int length=10;
+    int width=5;
+    int area;
+    area=length * width;
+    printf("area is= %d",area);
+    return 0;
+}
